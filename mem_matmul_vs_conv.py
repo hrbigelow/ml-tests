145c9e463b7fcb32f033a011d49c588fbd84ccd2
Henry Bigelow <hrbigelow@gmail.com>
2018-08-07 12:10:09 -0700

    updates

--- mem_matmul_vs_conv.py
+++ mem_matmul_vs_conv.py
@@ -1,21 +1,6 @@
 # Testing memory usage of 1x1 convolution vs matmul
 import tensorflow as tf
 
-I = 32 # num input channels
-O = 512 # num output channels
-B = 10 # batch size
-T = 1000 # time steps
-
-filt = tf.get_variable(shape=[I, O], name='filt')
-input = tf.random_normal(shape=[B, T, I], name='input')
-
-with tf.variable_scope('conv'):
-    conv = tf.nn.convolution(input, tf.expand_dims(filt, 0), 'VALID', [1], [1], 'conv')
-with tf.variable_scope('mat'):
-    # filt_exp = tf.broadcast_to(f, [7, 32, 512]) # can't use - no gradient op implemented
-    filt_exp = tf.stack([filt] * B)
-    mm = tf.matmul(input, filt_exp)
-
 def print_tensor_sizes(sess):
     '''
     for each tensor in the graph, print:
@@ -31,6 +16,26 @@ def print_tensor_sizes(sess):
 
             print('\t'.join(map(str, fields)))
 
+I = 32 # num input channels
+O = 512 # num output channels
+B = 10 # batch size
+T = 1000 # time steps
+
+with tf.variable_scope('zzzinput'):
+    filt = tf.get_variable(shape=[I, O], name='filt')
+    input = tf.random_normal(shape=[B, T, I], name='input')
+
+with tf.name_scope('cnv1x1'):
+    conv = tf.nn.convolution(input, tf.expand_dims(filt, 0), 'VALID', [1], [1], 'conv')
+with tf.name_scope('matmul'):
+    # filt_exp = tf.broadcast_to(f, [7, 32, 512]) # can't use - no gradient op implemented
+    filt_exp = tf.stack([filt] * B)
+    mm = tf.matmul(input, filt_exp)
+
+# Both approaches give the same result
 sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+print('conv equal to mm? ' + str(sess.run(tf.reduce_all(tf.equal(conv, mm)))))
+
 print_tensor_sizes(sess)
 

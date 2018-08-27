e55e7c60cec118f8387e10f5221fd5d90ad3a7e9
Henry Bigelow <hrbigelow@gmail.com>
2018-08-26 21:51:44 -0700

    cleaned up

--- fold.py
+++ fold.py
@@ -2,22 +2,6 @@
 import numpy as np
 import mmconv as mmc
 
-def joinl(vals, sep):
-    '''join a list of lists using sep_list'''
-    if len(vals) == 0: 
-        return None 
-    j = vals[0]
-    for l in vals[1:]:
-        j += sep + l
-    return j
-
-
-def joinv(val, rep, sep):
-    '''join rep repetitions of val using sep.
-    works if val and sep are scalars or lists'''
-    return (val + sep) * (rep - 1) + val 
-
-
 def pad(ary, dim, cnt, val):
     '''append ary along dim with cnt copies of val'''
     assert isinstance(ary, np.ndarray)
@@ -40,7 +24,6 @@ def linidx(shape, index):
     return index[-1] + shape[-1] * linidx(shape[:-1], index[:-1])
 
 
-
 class Fold(object):
     '''implement folding and unfolding between one and multiple
     dimensions'''
@@ -59,50 +42,7 @@ class Fold(object):
         self.dilation = dilation
 
 
-    def _i(self, d):
-        '''input width in the d-th dimension (from 0)'''
-        if d == 0:
-            return self._id[d]
-        else:
-            sep = self._f(d - 1) - 1
-            return joinv(self._i(d - 1), self._id[d], sep)
-
-
-    def _f(self, d):
-        '''filter width in the d-th dimension (from 0)'''
-        if d == 0:
-            return self._fd[d]
-        else:
-            sep = self._i(d - 1) - 1
-            return joinv(self._f(d - 1), self._fd[d], sep)
-
-    def _k(self, d):
-        '''position of the key element in the d-th dimension (from 0)'''
-        if d == 0:
-            return self._f(d) // 2
-        else:
-            sep = self._i(d - 1) - 1
-            return (self._f(d - 1) + sep) * (self._fd[d] // 2) + self._k(d - 1)
-
-
-    def _ism(self, d):
-        '''spacer mask for the input unfolding'''
-        if d == 0:
-            return [True] * self._i(d)
-        else:
-            sep = [False] * (self._f(d - 1) - 1)
-            return joinv(self._ism(d - 1), self._id[d], sep)
-
-    def _fsm(self, d):
-        '''spacer mask for the filter unfolding'''
-        if d == 0:
-            return [True] * self._fd[d]
-        else:
-            sep = [False] * (self._i(d - 1) - 1) 
-            return joinv(self._fsm(d - 1), self._fd[d], sep)
-
-
-    def input_spacer_mask(self):
+    def _input_spacer_mask(self):
         m = np.full(self._id, True)
         for d in reversed(range(1, self.ndim)):
             m = pad(m, d, self._fd[d] - 1, False)
@@ -113,7 +53,7 @@ class Fold(object):
         return m.reshape(-1)[:end + 1]
 
 
-    def unfold_filter(self, filter):
+    def _unfold_filter(self, filter):
         m = filter
         for d in reversed(range(1, self.ndim)):
             m = pad(m, d, self._id[d] - 1, 0)
@@ -124,7 +64,7 @@ class Fold(object):
         return m.reshape(-1)[:end + 1], key
 
 
-    def validity_mask(self):
+    def make_mask(self):
         v = np.array([True])
         for d in range(self.ndim):
             m, _ = mmc.make_mask(self._id[d], self._fd[d], self.stride[d], self.pad[d])
@@ -133,10 +73,10 @@ class Fold(object):
 
 
     def make_matrix(self, filter):
-        filter_vals, ki = self.unfold_filter(filter)
+        filter_vals, ki = self._unfold_filter(filter)
         filter_vals = filter_vals.tolist()
         tmp_filter_sz = len(filter_vals)
-        is_mask = self.input_spacer_mask()
+        is_mask = self._input_spacer_mask()
         tmp_matrix_sz = len(is_mask)
         loff = tmp_matrix_sz - ki - 1
         roff = tmp_matrix_sz - tmp_filter_sz + ki
@@ -154,7 +94,4 @@ class Fold(object):
         tmp_mat = np.array(cells).reshape(tmp_matrix_sz, tmp_matrix_sz)
         mat = tmp_mat[is_mask,:][:,is_mask]
         return mat
-
-
-
 
